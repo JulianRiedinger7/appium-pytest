2b59ca6945a6721944dd3301b808b1cd10b41a29
Julian Riedinger <julianriedinger27@gmail.com>
2024-05-30 13:49:41 -0300

    created invalid login test cases

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,6 +8,7 @@ class LoginPage(BasePage):
     USERNAME_INPUT = (AppiumBy.ACCESSIBILITY_ID, "test-Username")
     PASSWORD_INPUT = (AppiumBy.ACCESSIBILITY_ID, "test-Password")
     LOGIN_BUTTON = (AppiumBy.ACCESSIBILITY_ID, "test-LOGIN")
+    ERROR_MESSAGE = (AppiumBy.XPATH, "//*[@content-desc = 'test-Error message']/android.widget.TextView")
 
     def enter_username(self, username: str):
         self.enter_text(self.USERNAME_INPUT, username)
@@ -18,3 +19,6 @@ class LoginPage(BasePage):
     def click_login_button(self) -> ProductsPage:
         self.click_on(self.LOGIN_BUTTON)
         return ProductsPage(self.driver)
+
+    def get_error_message_text(self):
+        return self.wait_for_element(self.ERROR_MESSAGE).text
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,4 +1,4 @@
-import time
+import pytest
 
 from pages.login_page import LoginPage
 
@@ -6,6 +6,25 @@ valid_credentials = {
     "username": "standard_user",
     "password": "secret_sauce"
 }
+
+invalid_credentials = [
+    {
+        "username": "",
+        "password": "",
+        "message": "Username is required"
+    },
+    {
+        "username": "test",
+        "password": "",
+        "message": "Password is required"
+    },
+    {
+        "username": "test",
+        "password": "test",
+        "message": "Username and password do not match any user in this service."
+    }
+]
+
 
 def test_correct_login(driver):
     login_page = LoginPage(driver)
@@ -15,3 +34,13 @@ def test_correct_login(driver):
     products_page = login_page.click_login_button()
 
     assert products_page.get_products_title_text().lower() == "products"
+
+@pytest.mark.parametrize("credentials", invalid_credentials)
+def test_incorrect_login(driver, credentials):
+    login_page = LoginPage(driver)
+
+    login_page.enter_username(credentials.get("username"))
+    login_page.enter_password(credentials.get("password"))
+    login_page.click_login_button()
+
+    assert login_page.get_error_message_text() == credentials.get("message")
